8d3becd30e9db1b4adc11f843ad4ce218be1f610
cameron cheng <cameroncheng@gmail.com>
2020-06-05 21:00:14 -0700

    move assets folder into src

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import { NativeRouter, Switch, Route} from 'react-router-native'
-import Home from './components/Homepage'
-import Sessions from './components/Sessions'
-import Results from './components/Results'
-import Swiper from './components/Swiper'
+import Home from './src/components/Homepage'
+import Sessions from './src/components/Sessions'
+import Results from './src/components/Results'
+
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
