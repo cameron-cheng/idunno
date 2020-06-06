30c844716daad344ad5f1c586807a15d129df00c
akbartels <akunrau@gmail.com>
2020-06-05 17:52:52 -0700

    Add swiper into Sessions.js

--- components/Sessions.js
+++ components/Sessions.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import Swiper from './swiper';
 
 export default ({ history }) => {
   return(
 <View>
   <Text>This is the Sessions page </Text>
   <Text>Timer</Text>
-
-    <Button title="change page" onPress={() => history.push("/")}></Button>
+    <Swiper />
+    <Button 
+      title="change page" 
+      onPress={() => history.push("/")}>
+    </Button>
 </View>
   )};
