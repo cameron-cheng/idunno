1539b564fbf527a76f444c3c0f433360496e86a0
akbartels <akunrau@gmail.com>
2020-06-06 18:37:44 -0700

    start lobby card with react-native template

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { Container, Content, Card, CardItem, Header, Body } from 'native-base'
 
 export default ({ history }) => {
-  return(
-<View>
-  <Text>This is the Lobby page </Text>
-    <Button title="Homepage" onPress={() => history.push("/")}></Button>
-</View>
-  )};
+  return(  
+    <Container>
+      <Header />
+      <Content>
+        <Card>
+          <CardItem header>
+            <Text>NativeBase</Text>
+          </CardItem>
+          <CardItem>
+            <Body>
+              <Text>
+                //Your text here
+              </Text>
+            </Body>
+          </CardItem>
+          <CardItem footer>
+            <Text>GeekyAnts</Text>
+          </CardItem>
+        </Card>
+      </Content>
+    </Container>
+      
+  )
+};
+
+const styles = StyleSheet.create({
+
+  
+})
