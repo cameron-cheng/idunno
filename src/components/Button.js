f80982f9b2a7e945e4b9c7fcfd4607cc22e519ad
akbartels <akunrau@gmail.com>
2020-06-07 12:08:52 -0700

    Render lobby or swiper based on one ready player

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, Dimensions } from 'react-native';
+import Swiper from './Swiper';
+import Lobby from './Lobby';
+
+export default function Button(props) {
+
+  
+  // return (
+  //   <button onPress={props.onClick}>
+  //     {props.children}
+  //   </button>
+  // );
+
+
+}
--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { Container, Content, Card, CardItem, Header, Body } from 'native-base'
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { Container, Content, Card, CardItem, Header, Body, Button } from 'native-base';
+import { State } from 'react-native-gesture-handler';
 
-export default ({ history }) => {
+
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+export default function Lobby(props) {
+  // const [ready, setReady] = useState(false)
+  // console.log("PROPS", props)
+
+  function onReady() {
+    if (ready) {
+      setReady(false)
+    } else {
+      setReady(true)
+    }
+  }
+  
+  //just to see if it's working 
+  // function change() {
+  //   if (ready) {
+  //     return(<Text>TRUE</Text>)
+  //   } else {
+  //     return(<Text>FALSE</Text>)
+  //   }
+  // }
+  // const changeBool = change();
+  //
+  
   return(  
-    <Container>
+    <Container style={styles.container}>
       <Header />
-      <Content>
-        <Card>
-          <CardItem header>
-            <Text>NativeBase</Text>
+      <Content style={styles.content}>
+        <Card style={styles.card}>
+          <CardItem header style={styles.header}>
+            <Text style={styles.text}>Ready to start deciding?</Text>
           </CardItem>
-          <CardItem>
-            <Body>
-              <Text>
-                //Your text here
+          <CardItem >
+            <Body style={styles.body}>
+              <Text style={styles.text}>
+                Waiting for decision makers...
               </Text>
+              <View style={{flexDirection: 'row'}}>
+                <Button rounded success 
+                  onPress={(event) => props.handleReady(event)}
+                  // onChange={(event) => props.onChange(event.target.value)}
+                  style={styles.button}>
+                    <Text style={styles.buttonText}>Ready</Text>
+                </Button>
+                
+                <Button rounded danger style={styles.button}>
+                  <Text style={styles.buttonText}>Not Ready</Text>
+                </Button>
+              </View>
+              <View>
+                {/* {changeBool} */}
+              </View>
             </Body>
-          </CardItem>
-          <CardItem footer>
-            <Text>GeekyAnts</Text>
           </CardItem>
         </Card>
       </Content>
@@ -29,6 +68,40 @@ export default ({ history }) => {
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    width: SCREEN_WIDTH,
+    
+    
+  },
+  content: {
+    padding: 10,
+    top: 100,
+  },
+  card: {
+    borderRadius: 20,
+    height: 400,
+    alignItems: 'center'
+  },
+  header: {
+    borderTopLeftRadius:20, 
+    borderTopRightRadius:20,
+  },
+  text: {
+    fontSize: 25
+  },
+  body: {
+    alignItems: 'center',
+    top: 60
+  },
+  button: {
+    paddingLeft: 20, 
+    paddingRight: 20,
+  },
+  buttonText: {
+    color: 'white', 
+    fontWeight: 'bold', 
+    fontSize: 16,
+  },
   
 })
