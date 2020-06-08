b669e83772dadca30bb56cd5647dc24aa1c5487d
Jeremy <jeremy.nasato12@gmail.com>
2020-06-08 12:35:50 -0700

    set up api server routes and create axios request for googe places data

--- server/routes/invitation.js
+++
@@ -1,8 +0,0 @@
-const express = require ("express");
-const router = express.Router();
-
-router.get("/invitation", (req, res, next) => {
-  res.send("Invitation Route")
-}); 
-
-module.exports = router;
