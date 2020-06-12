5a50b7fbddc069448f035f51c52a55003edda73d
akbartels <akunrau@gmail.com>
2020-06-11 20:47:10 -0700

    complete home page layout. still need to move filters and join room

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -11,11 +11,11 @@ export default function Footer(props) {
   
   return (
     <View style={style.container}>
-        <Header span style={style.header}>
+        <View span style={style.header}>
           <Body style={style.body}>
-            <Title style={style.title}>¯\_(ツ)_/¯</Title>
+            <Title style={style.title}>iDUNNO</Title>
           </Body>
-        </Header>
+        </View>
     </View>
 
   );
@@ -25,22 +25,34 @@ export default function Footer(props) {
 const style = StyleSheet.create({
   container: {
     // flex: 1,
-    width: SCREEN_WIDTH,
-    // height: 80,
+    // width: SCREEN_WIDTH,
+    
+    // height: 10,
     justifyContent: 'center',
     shadowColor: '#ae9f77',
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.8,
     shadowRadius: 2,
+    // borderBottomLeftRadius: 600,
+    // borderBottomRightRadius: 100,
+
   },
   header: {
-    height: 80,
+    height: 800,
+    top: -420,
+    width: 1500,
     backgroundColor: '#b1d8d2',
+    borderBottomLeftRadius: 1000,
+    borderBottomRightRadius: 1000,
+    
     // backgroundImage: url("https://www.transparenttextures.com/patterns/asfalt-light.png")
   },
   body: {
     flex: 1,
-    justifyContent: 'center' 
+    justifyContent: 'flex-end' ,
+    paddingBottom: 20
+    // borderBottomLeftRadius: 600,
+    // borderBottomRightRadius: 100,
     
   },
   title: {
@@ -50,6 +62,10 @@ const style = StyleSheet.create({
     fontSize: 50,
     height: 60,
     fontWeight: '900',
+    // shadowColor: '#ae9f77',
+    // shadowOffset: { width: 3, height: 3 },
+    // shadowOpacity: 0.8,
+    // shadowRadius: 4,
     // fontFamily: 'AppleSDGothicNeo-Bold'
     
   },
--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
-import { Alert, View, TextInput, Button, StyleSheet } from 'react-native';
-import { Input } from 'react-native-elements';
+import { Alert, View, TextInput, Text, StyleSheet, Button, TouchableOpacity,  } from 'react-native';
+import { Input} from 'react-native-elements';
+// import { Button } from 'native-base';
 import Header from './Header';
 import Footer from './Footer';
 import Filters from './Filters';
 import Loader from './Loader';
+import LottieView from 'lottie-react-native';
+
+
 
 
 export default ({ history, createRoom, joinRoom, setRoomId, filters, setFilters }) => {
@@ -22,36 +26,83 @@ export default ({ history, createRoom, joinRoom, setRoomId, filters, setFilters 
   }
   
   return (
-    <View style={style.container}>
-      <Header />
-      <Filters state={filters} setState={setFilters}/>
-      <View style={{flex: 1, justifyContent: 'space-between'} }>
-        {/* <Button title="Filters" onPress={() => history.push("/filters")}></Button> */}
-        {/* <Button title="Room" onPress={() => history.push("/room")}></Button> */}
-        {/* <Button title="Results" onPress={() => history.push("/results")}></Button> */}
-        {/* <Button title="Invitation" onPress={() => history.push("/invitation")}></Button> */}
-        {/* <Button title="Lobby" onPress={() => history.push("/lobby")}></Button> */}
-        {/* <Button title="Login" onPress={() => history.push("/login")}></Button> */}
-        <Button title="Create Room" 
-          // onPress={createRoom }
-          onPress={handleCreateRoom}
-          // onPress={createRoom}
-          >
-            
-          </Button>
-        <Input title="Join!" onChangeText={text => setJoinRoomId(text)} value={joinRoomId} ></Input>
-        <Button title="Join Room" onPress={handleJoinRoom}></Button>
+    <View style={styles.container}>
+      <View style={{}}>
+        <Header />
       </View>
+      {/* ----- MAIN HOME ----- */}
+      {/* <View style={{  flex: 1,padding: 20, justifyContent: 'space-evenly', alignItems: 'center', }}>
+        <TouchableOpacity style={styles.buttons}><Text style={styles.buttonText}>NEW DECISION</Text></TouchableOpacity>
+        <Text style={styles.shrugger}>¯\_(ツ)_/¯</Text>
+        <TouchableOpacity style={styles.buttons}><Text style={styles.buttonText}>JOIN DECISION</Text></TouchableOpacity>
+      </View> */}
+      {/* ----- MAIN HOME ----- */}
 
-      <Footer />
+      <View style={{zIndex:10, }}>
+        <View style={{flex: 5}}>
+          <Filters state={filters} setState={setFilters} style ={{justifyContent: 'center',}}/>
+        </View>
+        <View style={{flex: 1} }>
+          <Button title="Create Room"
+            onPress={handleCreateRoom}
+            >
+          </Button>
+
+
+          {/* <Input title="Join!" onChangeText={text => setJoinRoomId(text)} value={joinRoomId} ></Input>
+          <Button title="Join Room" onPress={handleJoinRoom}></Button> */}
+        </View>
+      </View>
+      <View style={{}}>
+       <Footer />
+      </View>
+      
     </View>
  )};
+ 
+ {/* <Button title="Filters" onPress={() => history.push("/filters")}></Button> */}
+ {/* <Button title="Room" onPress={() => history.push("/room")}></Button> */}
+ {/* <Button title="Results" onPress={() => history.push("/results")}></Button> */}
+ {/* <Button title="Invitation" onPress={() => history.push("/invitation")}></Button> */}
+ {/* <Button title="Lobby" onPress={() => history.push("/lobby")}></Button> */}
+ {/* <Button title="Login" onPress={() => history.push("/login")}></Button> */}
 
-const style = StyleSheet.create({
+ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fcfaf2',
     flexDirection: 'column',
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  //home buttons
+  buttons: {
+    backgroundColor:'#ee937c', 
+    padding: 20, 
+    borderRadius: 10,
+    width: 300,
+    shadowColor: '#ae9f77',
+    shadowOffset: { width: 3, height: 4 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+  },
+  // home buttons
+  buttonText: {
+    fontSize: 30, 
+    color: '#fcfaf2', 
+    fontWeight: '800',
+    alignSelf: 'center'
+  },
+  shrugger: {
+    fontSize: 70,
+    color: '#2a9d8f',
+    fontWeight: '600',
+    shadowColor: '#ae9f77',
+    shadowOffset: { width: 3, height: 3 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    paddingBottom: 100,
+    paddingTop: 100
   }
 
 })
