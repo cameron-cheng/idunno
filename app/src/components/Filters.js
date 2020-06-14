8b4c10f2e7222df7c0aae157c51196c30fa9ccac
akbartels <akunrau@gmail.com>
2020-06-13 18:58:21 -0700

    tweaked filters styling

--- app/src/components/Filters.js
+++ app/src/components/Filters.js
@@ -1,21 +1,19 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, Dimensions, Picker, Slider,TouchableOpacity } from 'react-native';
 import { Input, Button, CheckBox, Overlay } from 'react-native-elements'
-import { Container } from 'native-base';
+import { Container, Card } from 'native-base';
 import Header from './Header';
 import Footer from './Footer';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 
-import { AppLoading } from 'expo';
-import { useFonts, Candal_400Regular } from '@expo-google-fonts/candal';
+
+
 
 export default function Filters(props){
 
-  let [fontsLoaded] = useFonts({
-    Candal_400Regular,
-  });
+  
 
   const [visible, setVisible] = useState(false)
   const [nickname, setNickname] = useState('')
@@ -36,9 +34,8 @@ export default function Filters(props){
   function renderSearchType() {
     if (filters.searchType === "nearby") {
       return (
-        <View style={{alignItems: 'stretch', justifyContent: 'center', height:40}}>
+        <View style={{alignItems: 'stretch', justifyContent: 'center', height:40, top: -30}}>
           <Slider
-            
             minimumValue='500'
             maximumValue='2000'
             step='10'
@@ -49,32 +46,30 @@ export default function Filters(props){
       )
     } else {
 
-      if (!fontsLoaded) {
-        return <AppLoading />;
-      } else {
-
       return (
-        <View style={{height:40}}>
+        <View style={{alignItems: 'stretch', justifyContent: 'center', height:40, top: -30}}>
         <Input style={styles.input} placeholder="Enter a location" onChangeText={text => setFilters({...filters, area: text.toLowerCase().trim()})} />
         </View>
       )
-    }
+    
   }
 }
   return (
 
     <Container style={styles.container}>
       <Header />
+      
         <View style={styles.filters}>
-          <View style={{width:300, height:530, justifyContent:'space-between', }}>
-            <View>
+        <Card style={styles.card}>
+          <View style={{width:300, justifyContent:'space-between', }}>
+            <View style={{justifyContent: 'center'}}>
             <View>
               <Picker
-              style={{}}
+              style={{top: -30}}
                 selectedValue={filters.searchType}
                 onValueChange={currentType => setFilters({...filters, searchType: currentType})} >
                 <Picker.Item label="Search Nearby" value="nearby" />
-                <Picker.Item label="Search by Area" value="text" />
+                <Picker.Item label="Search by Location" value="text" />
               </Picker>
             </View>
             {renderSearchType()}
@@ -86,24 +81,21 @@ export default function Filters(props){
                 onValueChange={value => setFilters({...filters, price: Math.ceil(value)})} />
               <Text>Price: {'$'.repeat(Math.ceil(filters.price)) || 'Free'}</Text>
             </View>
-            <TouchableOpacity onPress={toggleOverlay} style={{borderRadius: 10,}}>
+            <TouchableOpacity onPress={toggleOverlay} style={{top: -10}}>
               <Text style={{fontSize: 18, alignSelf: 'flex-end', color:'#ee937c', fontWeight: '500'}}>More Filters</Text>
             </TouchableOpacity>
             </View>
             
           
-            <View >
+            <View style={{justifyContent: 'center', top: 20}}>
               <Input style={{height:40}}placeholder="Host Name" onChangeText={text => setNickname(text.trim())} value={nickname} ></Input>
 
-              <TouchableOpacity onPress={handleCreateRoom} style={{backgroundColor: '#ee937c', borderRadius: 10,}}>
-                <Text style={{fontSize: 30, alignSelf: 'center', color:'#fcfaf2', fontWeight: '700', paddingVertical:10, fontFamily: 'Candal_400Regular' }}>Start Deciding!</Text>
+              <TouchableOpacity onPress={handleCreateRoom} style={styles.button}>
+                <Text style={styles.buttonText}>START DECIDING!</Text>
               </TouchableOpacity>
             </View>
-          
-            
-        
-          </View>
-
+            </View>
+            </Card>
           <Overlay overlayStyle={{ height: 470 }} isVisible={visible} onBackdropPress={toggleOverlay}>
             <View style={{flex: 1, flexDirection: 'column'}}>
               <CheckBox 
@@ -148,6 +140,7 @@ export default function Filters(props){
               />
             </View>
           </Overlay>
+        
         </View>
       <Footer />
     </Container>
@@ -171,7 +164,7 @@ const styles = StyleSheet.create({
     // paddingHorizontal: 60,
     // paddingBottom: 20,
     // backgroundColor: '#fff',
-    margin:50,
+    margin:40,
     
   
   },
@@ -179,6 +172,35 @@ const styles = StyleSheet.create({
     // flex: 1
   },
   button: {
-    // marginBottom: 5
+    backgroundColor:'#ee937c', 
+    padding: 10, 
+    borderRadius: 10,
+    width: 200,
+    shadowColor: '#ae9f77',
+    shadowOffset: { width: 3, height: 4 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    zIndex: 10,
+    alignSelf: 'center'
+  },
+  buttonText: {
+    fontSize: 20, 
+    color: '#fcfaf2', 
+    fontWeight: '800',
+    alignSelf: 'center',
+  },
+  card: {
+    borderRadius: 20,
+    alignItems: 'center',
+    backgroundColor: '#f9f1dc',
+    shadowColor: '#988a55',
+    shadowOffset: { width: 2, height: 4 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    padding: 20,
+    justifyContent: 'space-between',
+    width: 325,
+    height: 540,
+    top: 18
   }
 });
